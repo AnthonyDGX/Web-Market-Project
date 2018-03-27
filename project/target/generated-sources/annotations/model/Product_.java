352ad9793092b1d51fc46ebc95504e0979461af0
AnthonyDGX <dagneauxanthony@gmail.com>
2018-03-27 09:57:08 +0200

    :zap:
    Améliroation des commandes. Il reste a travailler sur le bug de l'ajout de la description. Une fois cela fait, la partie back du client sera finie.

---
+++ project/target/generated-sources/annotations/model/Product_.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import model.Manufacturer;
+import model.ProductCode;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-27T09:54:33")
+@StaticMetamodel(Product.class)
+public class Product_ { 
+
+    public static volatile SingularAttribute<Product, BigDecimal> purchaseCost;
+    public static volatile SingularAttribute<Product, ProductCode> productCode;
+    public static volatile SingularAttribute<Product, Integer> productId;
+    public static volatile SingularAttribute<Product, BigDecimal> markup;
+    public static volatile SingularAttribute<Product, Integer> quantityOnHand;
+    public static volatile SingularAttribute<Product, String> available;
+    public static volatile SingularAttribute<Product, Manufacturer> manufacturerId;
+    public static volatile SingularAttribute<Product, String> description;
+
+}
