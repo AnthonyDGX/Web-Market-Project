a13c13d83e10ce2fd755fb792fb4fb0838c01633
Peltier10 <34452954+Peltier10@users.noreply.github.com>
2018-04-04 09:39:33 +0200

    :bug:

--- project/target/generated-sources/annotations/model/Product_.java
+++ project/target/generated-sources/annotations/model/Product_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Manufacturer;
 import model.ProductCode;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-02T17:34:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-04T09:29:50")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
