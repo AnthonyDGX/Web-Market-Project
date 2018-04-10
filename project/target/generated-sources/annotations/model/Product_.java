7729b78f3d7a20fcd04f723cbeca742cef37034f
AnthonyDGX <dagneauxanthony@gmail.com>
2018-04-10 16:17:02 +0200

    :+1: Design page user

--- project/target/generated-sources/annotations/model/Product_.java
+++ project/target/generated-sources/annotations/model/Product_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Manufacturer;
 import model.ProductCode;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-10T08:59:28")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-10T16:02:05")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
